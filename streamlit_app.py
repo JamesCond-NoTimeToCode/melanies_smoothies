b900d17bceef4f5be88289c851d20784cefd34df
JamesCond-NoTimeToCode <srikanth.vy1225@gmail.com>
2025-02-14 16:45:00 -0600

    Update streamlit_app.py
    moved 3 to 18 
    
    
    from snowflake.snowpark.functions import col

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col
+
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
@@ -15,6 +15,7 @@ import streamlit as st
     ##st.write("your favorite fruit is:", option)
 
 
+from snowflake.snowpark.functions import col
 
 session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select (col('Fruit_Name'))
@@ -43,4 +44,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-
