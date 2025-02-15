da59d1586ea77e40b2b03db3d733bc39612a116d
JamesCond-NoTimeToCode <srikanth.vy1225@gmail.com>
2025-02-14 22:19:03 -0600

    Update streamlit_app.py
    add 20 
    
    cnx = st.connection(“snowflake”)
    session = cnx.session()

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,9 @@ import streamlit as st
 #st.write("Your favorite fruit is:", option)
 
 from snowflake.snowpark.functions import col
-session = get_active_session()
+
+cnx = st.connection(“snowflake”)
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
