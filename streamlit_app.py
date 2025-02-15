10790f5328c3aadcc1897ee59a2a8f70af92f49f
JamesCond-NoTimeToCode <srikanth.vy1225@gmail.com>
2025-02-14 22:29:02 -0600

    Update streamlit_app.py
    working

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,55 +1,52 @@
 # Import python packages
 import streamlit as st
 
-
 cnx = st.connection('snowflake')
 session = cnx.session()
+
 # Write directly to the app
-st.title(":cup_with_straw: Customize Your Smoothie:cup_with_straw:")
+st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
-    """Choose the fruits you want in your custom smoothie!
+    """choose the fruits you want in your custom smoothie! :apple: :pineapple:
     """
 )
+
 import streamlit as st
 
-#option = st.selectbox(
- #   "What is your favorite fruit?",
-  #  ("Banana", "Strawberries", "Peaches"),
-#)
+    ##option = st.selectbox("what is your favorite fruit",("Banana", "Strawberries", "peaches"), )
+    
+    ##st.write("your favorite fruit is:", option)
 
-#st.write("Your favorite fruit is:", option)
 
 from snowflake.snowpark.functions import col
 
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select (col('Fruit_Name'))
+##st.dataframe(data=my_dataframe, use_container_width=True)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-
-
-ingredients_list= st.multiselect(
-    'Choose up to 5 ingredients:'
-    , my_dataframe
-    
+ingredients_list = st.multiselect(
+    "choose upto 5 ingredients",
+    my_dataframe 
 )
+
 if ingredients_list:
     #st.write(ingredients_list)
-    #st.text(ingredients_list)
-    ingredients_string = ''
+    #st.text (ingredients_list)
+    ingredients_string=''
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen
-    st.write(ingredients_string)
+        ingredients_string += fruit_chosen + ' '
+    #st.write (ingredients_string)
+
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
             values ('""" + ingredients_string + """')"""
-    time_to_insert = st.button('Submit order')
+
+    #st.write(my_insert_stmt)
+
+    time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-
-        st.success('Your Smoothie is ordered!',icon ="✅")
-
-
-
-
+        st.success('Your Smoothie is ordered!', icon="✅")
 
 
 
