143f883a6a032fe5349665e85e5ee97eba41879b
JamesCond-NoTimeToCode <srikanth.vy1225@gmail.com>
2025-02-15 18:28:45 -0600

    Update streamlit_app.py
    added 
    
    import requests
    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
    st.text(smoothiefroot_response)

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,6 +55,10 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
+##New section to improve smoothie nutrition information
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
          
